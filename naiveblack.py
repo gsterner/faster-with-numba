371c33231dba4edbad318a23e86b9e283e8710bf
Gustaf Lundborg <gsterner@gmail.com>
2023-02-06 21:42:41 +0100

    Add calc with only basic python

--- naiveblack.py
+++ naiveblack.py
@@ -1,16 +1,17 @@
-import numpy as np
+import random
+import math
 import time
 #from numba import jit
 
 #@jit(nopython=True)
 def black_loop(N, S_zero, T, r, sigma, K):
-    Z = np.random.normal(0, 1, size=(N))
+    Z = [random.normalvariate(0.0, 1.0) for n in range(N)]
     C_total = 0
     for z in Z:
-        S = S_zero * np.exp((r - 0.5 * sigma * sigma) * T + sigma * np.sqrt(T) * z)
-        C = np.maximum(0, S - K)
+        S = S_zero * math.exp((r - 0.5 * sigma * sigma) * T + sigma * math.sqrt(T) * z)
+        C = max(0, S - K)
         C_total = C_total + C
-    return np.exp(- r * T) * C_total / N
+    return math.exp(- r * T) * C_total / N
 
 maturity = 1
 underlying_start = 100
@@ -19,6 +20,7 @@ rate = 0.02
 volatility = 0.2
 #scenarios = 100000000 # 8
 scenarios = 1000000 # 6
+
 
 start = time.perf_counter()
 price = black_loop(scenarios, underlying_start, maturity, rate, volatility, strike)
